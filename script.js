da9967e818ab5f3a85f01c8fa96a6904a3885315
ahnafalrahi <138254166+ahnafalrahi@users.noreply.github.com>
2023-12-23 17:54:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// function circleMouseFollower(){
+//     window.addEventListener("mousemove", function(dets){
+//         document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px, ${dets.clientY}px)`;
+
+//     })
+// }
+
+// circleMouseFollower();
+
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav",{
+        y:'10',
+        opacity:0,
+        duration:1.5,
+        ease: Expo.easeInOut
+    })
+
+    .to(".boundingelem",{
+        y:0,
+        ease :Expo.easeInOut,
+        duration:1.6,
+        stagger:.2
+    })
+
+    .from("#herofooter",{
+        y:-10,
+        opacity:0,
+        duration:1.5,
+        delay:-1,
+        ease:Expo.easeInOut
+    })
+}
+
+var timeout
+
+function circleshapechange(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove",function(dets){
+        clearTimeout(timeout);
+
+        xscale = gsap.utils.clamp(.8,1.2,dets.clientX - xprev)
+        yscale = gsap.utils.clamp(.8,1.2,dets.clientX - yprev)
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        circleMouseFollower(xscale,yscale)
+        timeout = setTimeout(function(){
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px) scale(1,1)`
+        },100)
+       
+    }) 
+}
+circleshapechange();
+
+function circleMouseFollower(xscale,yscale){
+    window.addEventListener("mousemove" , function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale},${yscale})`
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
+
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot = 0;
+    elem.addEventListener("mouseleave",function(dets){
+        gsap.to(elem.querySelector("img"),{
+            opacity:0,
+            ease : Power3, 
+            // duration:0.5,          
+        })
+    })
+})
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot = 0;
+    elem.addEventListener("mousemove",function(dets){
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX
+        gsap.to(elem.querySelector("img"),{
+            opacity:1,
+            ease : Power3,
+            top:diff,
+            left:dets.clientX,
+            rotate: gsap.utils.clamp(-20,20,diffrot * 0.5)
+
+            
+        })
+    })
+})
